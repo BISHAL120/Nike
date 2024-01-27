d616d136305cea73b815d03c1d52cb7afc92d9f2
BISHAL120 <96854755+BISHAL120@users.noreply.github.com>
2024-01-27 23:33:00 +0600

    vercel SpeedInsights installed

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/header";
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
